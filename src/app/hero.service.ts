4442b99273b8120b5dc2532445f9d02bf9bed33f
yTsunomoto <wasiwasimaru@gmail.com>
2019-08-11 00:06:25 +0900

    update 更新。

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,24 @@ import {Injectable} from '@angular/core';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() {
+  constructor(private messageService: MessageService) {
   }
 
   getHeros(): Observable<Hero[]> {
+    // TODO update
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetch hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
